8182877fe97987c77ef5dd9db7963f791511c9bf
tarturr <tartur.dev@gmail.com>
2024-11-20 01:21:59 +0100

    Created project

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+group = "fr.tartur"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("dev.folia:folia-api:1.20.4-R0.1-SNAPSHOT")
+
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
