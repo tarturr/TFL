8b518f6d8deb4e176411517bb72ed666e261180e
tarturr <tartur.dev@gmail.com>
2024-11-25 23:51:12 +0100

    Added an isEmpty() method to the CommandPattern class and linked a builder to it.

--- src/main/java/fr/tartur/tfl/command/CommandPattern.java
+++ src/main/java/fr/tartur/tfl/command/CommandPattern.java
@@ -1,18 +1,22 @@
 package fr.tartur.tfl.command;
 
 import fr.tartur.tfl.command.node.IPatternNode;
+import lombok.Builder;
+import lombok.Singular;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@Builder
 public class CommandPattern {
 
-    private final Map<Integer, IPatternNode<?>> pattern;
+    @Singular
+    private final Map<Integer, IPatternNode<?>> patterns;
 
-    public CommandPattern(Map<Integer, IPatternNode<?>> pattern) {
-        this.pattern = pattern;
+    public CommandPattern(Map<Integer, IPatternNode<?>> patterns) {
+        this.patterns = patterns;
     }
 
     public CommandPattern() {
@@ -20,7 +24,7 @@ public class CommandPattern {
     }
 
     public Optional<? extends CommandNode<?>> match(CommandContext context) {
-        final IPatternNode<?> pattern = this.pattern.get(context.position());
+        final IPatternNode<?> pattern = this.patterns.get(context.position());
 
         if (pattern != null) {
             final var node = pattern.match(context);
@@ -34,13 +38,17 @@ public class CommandPattern {
     }
 
     public List<String> complete(CommandContext context) {
-        final IPatternNode<?> pattern = this.pattern.get(context.position());
+        final IPatternNode<?> pattern = this.patterns.get(context.position());
 
         if (pattern != null) {
             return pattern.complete(context);
         }
 
         return List.of();
+    }
+
+    public boolean isEmpty() {
+        return this.patterns.isEmpty();
     }
 
 }
